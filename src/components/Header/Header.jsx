4cbf5cc2a2e897c3c28fa921b0744540a036fea4
Hanif <mohammadabuhanif95@gmail.com>
2025-12-05 06:25:18 +0800

    #changes the code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 const Header = () => {
@@ -28,34 +27,3 @@ const Header = () => {
 };
 
 export default Header;
-=======
-import React from "react";
-
-const Header = () => {
-  return (
-    <header>
-      <div className="top-strip py-2">
-        <div className="container">
-          <div className="flex item-center justify-between">
-            <div className="col1 w-[50%]">
-              <p className="text-[15px] font-normal">
-                💫 Your Season, Your Style:
-                <span className="font-bold text-primary">50%</span> off new
-                arrivals
-              </p>
-            </div>
-
-            <div className="col2 flex items-center justify-end">
-              <ul>
-                <li></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
-};
-
-export default Header;
->>>>>>> ee09bd2cf8eac413efbd2b076ab5f139faaf6e5a
